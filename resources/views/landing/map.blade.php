a60d02bd2f803423e324e93237fff3dadc9762ba
Marty Pennington <penningtonimages@gmail.com>
2020-02-21 08:55:35 -0700

    small changes

--- resources/views/landing/map.blade.php
+++ resources/views/landing/map.blade.php
@@ -13,10 +13,15 @@
         <p class="lead">3915 47 Ave, Lloydminster SK S9V 2C7</p>
         <br>
         <h2 class="display-4 mx-auto text-center">Directions</h2>
+        <div class="list-group ml-n4 col-lg-6 p-5">
         <p class="lead">Pleasantview Bible Camp is located 20 km north of Lloydminster on the northeast side of Sandy Beach lake.</p>
         <p class="lead">After turning off Highway 17 take your immediate left and continue for approximately 500m and you will see the enterance to the camp on your right.</p>
-
+        </div>
         <br>
+
+
+
+
 
       </div>
       <div class="embed-responsive embed-responsive-4by3 col-lg-6 ">
