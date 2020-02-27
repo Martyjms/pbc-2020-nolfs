ad76a23b535cf40e262cc7c6425e643832e1132e
Marty Pennington <penningtonimages@gmail.com>
2020-02-27 12:07:35 -0700

    removed murdoc

--- resources/views/director.blade.php
+++ resources/views/director.blade.php
@@ -8,7 +8,7 @@
       <div class=" col-lg-6">
 
 
-        <h2>KENTON GIESBRECHT (MURDOC)
+        <h2>KENTON GIESBRECHT
         </h2>
         <p class="lead">Born in Manitoba, Kenton is a die-hard Jets fan! He moved to Lloydminster in 1983 and has been involved at First Baptist Church ever since. Kenton is happily married to Sandi, who he met at bible camp when he was younger. They have four children and 17 grandchildren.</p>
         <p class="lead">Kenton has a huge heart for camp ministry and has been the director at Pleasantview ever since 2010. His favorite camp activity is horsemanship and he also has some amazing headstand skills!</p>
