4115d40ef278ce68c483f7081a80323dcf8295dd
Marty Pennington <penningtonimages@gmail.com>
2020-02-27 12:45:40 -0700

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
     <title>@yield('title', 'PBC 2020')</title>
 
 <!-- ---------------------------------------------------------
