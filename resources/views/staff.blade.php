0c65b7961f0d6d841dffc25b92408212473310e2
Marty Pennington <penningtonimages@gmail.com>
2020-03-24 11:41:09 -0600

    staff page changes

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -26,7 +26,7 @@
   <ul class="list-group ml-n4 col-lg-6 p-5">
         <h3 class="text-center">PBC Staff Application Forms</h3>
         <li class="list-group-item"><a href="{{asset('pdf/FBC MINISTRY APPLICATION FORM - PLEASANTVIEW BIBLE CAMP 2020.pdf')}}" download>FBC Ministry Application Form - PBC 2020</a></li>
-        <li class="list-group-item"><a href="{{asset('pdf\FBC MINISTRY APPLICATION FORM - PLEASANTVIEW BIBLE CAMP 2020.pdf')}}" download>FBC Ministry Application Form - Adult 2020</a></li>
+        <li class="list-group-item"><a href="{{asset('pdf\FBC MINISTRY APPLICATION FORM - ADULT 2020.pdf')}}" download>FBC Ministry Application Form - Adult 2020</a></li>
         <li class="list-group-item"><a href="{{asset('pdf\FBC MINISTRY APPLICATION FORM - YOUTH 2020.pdf')}}" download>FBC Ministry Application Form - Youth 2020</a></li>
         <li class="list-group-item"><a href="{{asset('pdf\FBC VOLUNTEER COVENANT 2020.pdf')}}" download>FBC Volunteer Covenant 2020</a></li>
         <li class="list-group-item"><a href="{{asset('pdf\PBC REFERENCE FORM 2020.pdf')}}" download>PBC Reference Form 2020</a></li>
@@ -46,6 +46,7 @@
                 <p class="lead">The Leader in Training (LIT) program is the first step toward becoming a full time staff member at Pleasantview. It is an awesome program that is designed for teens who will be 15 or older in 2020, who are eager for christ-centered discipleship and have a desire to learn more about camp ministry!</p>
                 <p class="lead">The LIT program runs during the Preteen 1 and Junior High 1 weeks of camp (July 12-17, July 19-24), after which you will serve at camp for at least one of the following three weeks. The program will not only challenge you in your personal relationship with Christ, but will also equip you to become an effective camp staff member so that you can impact the lives of others!</p>
                 <p class="lead">The cost of the program is $310, which is due upon acceptance of your application. This fee covers everything including accommodations, food, and study materials. Please note that space is limited, so please apply soon!</p>
+                <p class="lead">This year's LIT leaders are Haylee Langille and Toryne O'Doherty.</p>
                 <p class="lead">Please click on the links below to download the lit application forms. If you have not filled out the FBC volunteer application, you will also need to complete that along with the attached reference forms​.</p>
                 <p id="due-dates">LIT application forms due April 1</p>
                 </div>
