02fc61be6280d3ecefdd88e73ff6e625b67f782c
Marty Pennington <penningtonimages@gmail.com>
2021-04-14 10:57:57 -0600

    change favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('images/PBC-LOGO.ico') }}">
     <title>@yield('title', 'Pleasantview Bible Camp')</title>
 
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - -
