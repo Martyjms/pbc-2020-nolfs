b1636a26b930e1aa93ebacb03a867cdeccbe9624
Marty Pennington <penningtonimages@gmail.com>
2022-03-10 07:11:22 -0700

    more updates v2

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -108,9 +108,9 @@
   </div>
 
 
-  <div class="container-fluid padding">
+  {{-- <div class="container-fluid padding">
     <img src="{{asset('images\Camp_logo_2022_peace.jpg')}}" class="img-fluid" alt="">
-  </div>
+  </div> --}}
 
 <div class="camps container">
     <div class="d-flex flex-row flex-wrap justify-content-center">
@@ -126,8 +126,8 @@
                     </div>
                     <div class="d-flex justify-content-between align-items-center">
                     <a href="/register" class="stretched-link">Registration</a>
-                    <p>Grades 10 - 12</p>
-                    <p>& 2022 Graduates</p>
+                    <p>Grades 10-12 & 2022 Grads</p>
+
                     {{-- <p>$320</p> --}}
                     </div>
                 </div>
